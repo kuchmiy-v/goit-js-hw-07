3f45d317fba3f107b5e797e273139e166b558353
kuchmiy-v <wrdvrk@gmail.com>
2023-05-10 21:17:24 +0300

    first tray

---
+++ js/01-gallery.js
@@ -0,0 +1,41 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+
+// galleryList.insertAdjacentHTML("beforeend", galleryMarkup);
+
+const galleryMarkup = galleryItems
+  .map(
+    (item) => `
+<li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</li>
+`
+  )
+  .join("");
+galleryList.innerHTML = galleryMarkup;
+
+galleryList.addEventListener("click", onGalleryClick);
+
+function onGalleryClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.sourse}" width="800" height="600">`
+  );
+
+  instance.show();
+}
